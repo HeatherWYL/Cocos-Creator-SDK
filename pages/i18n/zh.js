0dbecc04db36902315b7f0824a8b7b81069ac7a7
xin.yang <953459485@qq.com>
2020-11-24 18:43:43 +0800

    更新版本号，修改提示信息提示H5不支持视频模块

--- pages/i18n/zh.js
+++ pages/i18n/zh.js
@@ -2,6 +2,7 @@ module.exports = {
     "miss_param_tips": "请在 Service 面板设置好 CocosAds-Test 的 AppID ",
     "service_enable": "服务已开启，已经将 Agora SDK 安装到您项目的 assets/agora 目录中，请参考使用指南 API 文档调用相关功能接口。",
     "service_tips": "注：声网 HTML5 平台暂不支持 iOS 设备中的 iPod Touch 产品。",
+    "service_tips2": "注：声网 HTML5 平台仅支持音频版本",
     "api_level_tips": "声网服务在构建 Android 平台时要求 Target API Level 最低为 android-23，否则可能会造成构建失败。",
     "save_param_success": "保存参数成功!",
     "sdk_type": "SDK 类型",
